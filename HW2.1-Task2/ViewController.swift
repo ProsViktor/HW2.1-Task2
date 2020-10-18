109a744a4d08ae0f39e7611bef714178467096c9
Viktor Prosvetov <viktor.prosvetov@all.me>
2020-10-18 18:03:42 +0300

    Initial Commit

---
+++ HW2.1-Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1-Task2
+//
+//  Created by Viktor Prosvetov on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
